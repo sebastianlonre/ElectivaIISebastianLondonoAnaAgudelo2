800eb21dfda3cea1208bc0ab49574bd3e2aeb97b
ANAISA123 <ana.agudelo95@correo.tdea.edu.co>
2024-10-11 00:42:51 -0500

    ADD route to Logout and a section is created to manage the protected route that is accessed only to view the currently logged in user.

--- shared/routes/apiRoutes.js
+++ shared/routes/apiRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { newTweet, listTweetsByIDs, listTweetsInFeeds } = require("../../tweets/infraestructure/tweetsController");
-const { registerUserItem, loginUserItem } = require("../../auth/infraestructure/authController");
+const { registerUserItem, loginUserItem, logoutUserItem } = require("../../auth/infraestructure/authController");
 const { followUsers } = require("../../social/infraestructure/socialController");
 const { unfollowUsers } = require("../../social/infraestructure/socialController");
 const { findUsersByTag, getFollowers } = require("../../users/infraestructure/userController");
 
+const checkAuthentication = (req, res, next) => {
+    if (req.session.user) {
+      next(); 
+    } else {
+      res.status(401).json({ message_error: "[ERROR] Not authenticated Users currently" });
+    }
+  };
+  
 //tweets
 router.post("/tweets", newTweet);
 router.get("/tweets/:userTag", listTweetsByIDs);
@@ -14,6 +22,13 @@ router.post("/tweets/feed", listTweetsInFeeds)
 //Auth
 router.post("/auth/register", registerUserItem);
 router.post('/login', loginUserItem);
+router.post('/logout', logoutUserItem);
+
+// Ruta protegida, solo puede acceder si el login es exitoso
+router.get('/protected-route', checkAuthentication, (req, res) => {
+    res.json({ message: "You are authenticated", user: req.session.user.userTag });
+  });
+  
 
 //social
 router.put("/social/follow", followUsers)
